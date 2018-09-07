9cbe8e7e45e99eb208e444ce0960dd2038838858
Jaeseok Huh <jaeseok@hanyang.ac.kr>
2018-09-07 11:34:30 +0900

    Remove dependency on working directory

--- models/ops/depthavgpooling/make.sh
+++ models/ops/depthavgpooling/make.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+cd "$(dirname ${BASH_SOURCE[0]})"
 cd src
+pwd
 nvcc -c -o depthavgpooling_cuda_kernel.cu.o depthavgpooling_cuda_kernel.cu -x cu -Xcompiler -fPIC -std=c++11 -arch=sm_60
 cd ..
-CC=g++ python build.py
+CC=g++ python3 build.py
