ca065df1552dd65082b4340b9eb20ad8d936ebe8
Jaeseok Huh <jaeseok@hanyang.ac.kr>
2018-09-08 16:40:05 +0900

    Fix the location of lists

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,10 @@
+cwd=$(pwd)
+cd ..
 python3 test.py \
 --gpu_ids 2 \
 --name nyuv2_VGGdeeplab_depthconv \
 --dataset_mode nyuv2 \
 --depthconv \
---list dataset/lists/nyuv2/test.lst \
+--list ./lists/nyuv2/test.lst \
 --how_many 0
+cd $cwd
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,8 +1,12 @@
+cwd=$(pwd)
+cd ..
 python3 train.py \
+--model DeeplabVGG \
 --name nyuv2_VGGdeeplab_depthconv \
 --dataset_mode nyuv2 \
 --flip --scale --crop --colorjitter \
 --depthconv \
---list ./lists/train.lst \
---vallist ./lists/val.lst \
+--list ./lists/nyuv2/train.lst \
+--vallist ./lists/nyuv2/val.lst \
 --continue_train
+cd $cwd
